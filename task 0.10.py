778b45737fd8b43d6dbe7f62a80c47a27caeae99
Muhluri97 <77506490+Muhluri97@users.noreply.github.com>
2021-03-24 15:59:20 +0200

    Update task 0.10.py

--- task 0.10.py
+++ task 0.10.py
@@ -2,4 +2,4 @@ def letters (word1, word2):
     alphabet = 'abcdefghijklmnopqrstuvwxyz, ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     for letter in alphabet:
         if letter in word1 and letter in word2:
-            print (letter)
+            print (letter, end = " ")
